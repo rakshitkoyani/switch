1c05186e81fe346af19dba9cc4fde94fec2c4040
Rakshit koyani <36806953+rakshitkoyani@users.noreply.github.com>
2022-10-20 16:34:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Rakshit</h1>
+      <h2>This is new framework</h2>
+    </div>
+  );
+}
